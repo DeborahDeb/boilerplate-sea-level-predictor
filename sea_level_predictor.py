fdf3bfb9224e910a5d8b296ae37a6f39fa704b5e
Deborah <36170648+DeborahDeb@users.noreply.github.com>
2025-12-31 19:23:54 -0300

    Implement sea level prediction plot with CSV data
    Added functionality to read data from a CSV file and plot sea level predictions with two lines of best fit.

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,20 +4,36 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], c='b', label='Original Data')
 
+    # Create first line of best fit (using all data)
+    res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_pred = pd.Series([i for i in range(1880, 2051)])
+    y_pred = res.slope * x_pred + res.intercept
+    plt.plot(x_pred, y_pred, 'r', label='Fitted Line 1880-2050')
 
-    # Create first line of best fit
-
-
-    # Create second line of best fit
-
+    # Create second line of best fit (using data from 2000 onwards)
+    df_recent = df[df['Year'] >= 2000]
+    res_recent = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    x_pred_recent = pd.Series([i for i in range(2000, 2051)])
+    y_pred_recent = res_recent.slope * x_pred_recent + res_recent.intercept
+    plt.plot(x_pred_recent, y_pred_recent, 'green', label='Fitted Line 2000-2050')
 
     # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend()
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
+
+# Call the function to run it
+if __name__ == "__main__":
+    draw_plot()
+    plt.show()
